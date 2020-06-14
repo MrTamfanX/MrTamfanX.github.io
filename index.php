64ac2e434b28bebd2250e3e04ac2baf3d5b843d3
MrTamfanX <50101968+MrTamfanX@users.noreply.github.com>
2020-06-15 01:55:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,450 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+     <meta name="viewport" content="Selamat datang di website resmi MrTamfanX Cyber Team Official"/>
+     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+     <meta property="og:images:type" content="images/perl2.jpg"/>
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">   
+     <meta name="keywords" content="www.mrtamfanxcyberteam.my.id"/>
+     <meta name="description" content="Selamat datang di website resmi MrTamfanX Cyber Team Official"/>
+     <link rel="icon" href="https://1.bp.blogspot.com/-E6T39kGP38k/XrJDN7oid_I/AAAAAAAAARA/rbwzYs1O_-waumNspgr5PJeST91DZNcTACLcBGAsYHQ/s1600/favicon.png" type="image/png">
+     <meta name="author" content="Prakasa Jr64">
+     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+     <meta name="viewport" content="width=device-width"/>
+     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+     <meta name="viewport" content="width=device-width"/>
+     <link href="assets/img/favicon.png" rel="icon">
+     <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <title>MrTamfanX Cyber Team Official</title>
+  <meta content="Selamat datang di website resmi MrTamfanX Cyber Team Official" name="descriptison">
+  <meta content="www.mrtamfanxcyberteam.my.id" name="keywords">
+
+  <!-- Favicons -->
+  
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+<script data-ad-client="ca-pub-3929343597183439" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+  </head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <div id="topbar" class="d-none d-lg-flex align-items-center fixed-top ">
+    <div class="container d-flex">
+      <div class="contact-info mr-auto">
+        <i class="icofont-envelope"></i> <a href="mailto:admin@mrtamfanxcyberteam.my.id">admin@mrtamfanxcyberteam.my.id</a>
+        <i class="icofont-phone"></i> +62 857-8041-1404
+      </div>
+      <div class="social-links">
+        <a href="wa.me/+62857-8041-1404" class="twitter"><i class="icofont-twitter"></i></a>
+        <a href="https://www.facebook.com/profile.php?id=100038754743614" class="facebook"><i class="icofont-facebook"></i></a>
+        <a href="https://bigsta.net/account/prakasa_jr64/" class="instagram"><i class="icofont-instagram"></i></a>
+        <a href="wa.me/+62857-8041-1404" class="skype"><i class="icofont-skype"></i></a>
+        <a href="https://github.com/MrTamfanX" class="github"><i class="icofont-github"></i></i></a>
+      </div>
+    </div>
+  </div>
+<script data-ad-client="ca-pub-3929343597183439" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script data-ad-client="ca-pub-3929343597183439" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script data-ad-client="ca-pub-3929343597183439" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+
+      <h5 class="logo mr-auto"><a href="index.php">MrTamfanX Cyber Team</a></h5>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.php" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.php">Home</a></li>
+          <li><a href="http://www.blog.mrtamfanxcyberteam.my.id">BLOG</a></li>
+          <li><a href="www.tools.mrtamfanxcyberteam.my.id">TOOLS</a></li>
+          <li><a href="#services">Informasi</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#team">Admin</a></li>
+          </ul>
+          </li>
+          <li><a href="#contact">Contact</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div><script data-ad-client="ca-pub-3929343597183439" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script data-ad-client="ca-pub-3929343597183439" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script data-ad-client="ca-pub-3929343597183439" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+    <div class="container position-relative" data-aos="fade-up" data-aos-delay="500">
+      <h1>Selamat Datang Teman</h1>
+      <h2>Di Website Resmi MrTamfanX Cyber Team</h2>
+      <a href="#about" class="btn-get-started scrollto">Selanjutnya</a>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left">
+            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right">
+            <h3>MrTamfanX Cyber Team</h3>
+            <p class="font-italic">
+              Di dirikan pada tahun 2018, Dan di minati oleh banyak orang, karena ke asik-kan admin²nya, dan sebagai berikut :
+            </p>
+            <ul>
+              <li><i class="icofont-check-circled"></i> Selalu mengasih materi yang admin punya dan admin bisa.</li>
+              <li><i class="icofont-check-circled"></i> Mampu mengatasi membernya dalam bidang IT.</li>
+              <li><i class="icofont-check-circled"></i> Menciptakan sebuah tools untuk bisa di pakai oleh orang lain.</li>
+            </ul>
+            <p>
+              Dan sebenarnya masih banyak lagi, Di team cyber mrtamfanx ini, Semua yang baru masuk itu di anggap sama dan kaga ada namanya yang lebih pro atau noob, Karena disini kita selalu belajar bersama.
+              Disini juga admin selalu mengasih materi tentang :
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Why Us Section ======= -->
+    <section id="why-us" class="why-us">
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-lg-4" data-aos="fade-up">
+            <div class="box">
+              <span>01</span>
+              <h4>Aplikasi</h4>
+              <p>Bagaimana caranya membuat aplikasi yang mudah untuk yang ingin belajar atau masih pemula.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="150">
+            <div class="box">
+              <span>02</span>
+              <h4>Sebuah Tools</h4>
+              <p>Bagimana caranya membuat tools dengan mudah dan bisa di pakai oleh banyak orang.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="300">
+            <div class="box">
+              <span>03</span>
+              <h4>Ngoding</h4>
+              <p>Bagaimana caranya belajar ngoding untuk yang ingin belajar atau masih pemula.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Why Us Section -->
+
+    <!-- ======= Clients Section ======= -->
+    <section id="clients" class="clients">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="row d-flex align-items-center">
+
+          <div class="col-lg-2 col-md-4 col-6">
+            <img src="assets/img/clients/client-1.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6">
+            <img src="assets/img/clients/client-2.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6">
+            <img src="assets/img/clients/client-3.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6">
+            <img src="assets/img/clients/client-4.png" class="img-fluid" alt="">
+          </div>
+
+          </div>
+
+      </div>
+    </section><!-- End Clients Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container">
+
+        <div class="section-title">
+          <span>Informasi</span>
+          <h2>Informasi</h2>
+          <p>Di bawah ini kalian bisa baca baca informasi dari kami.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="150">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4><a href="">Download File</a></h4>
+              <p>Jika kalian ingin download sebuah file yang berisi tools dari kami, tinggal visit blog.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="300">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-slideshow"></i></div>
+              <h4><a href="">Chanel YouTube</a></h4>
+              <p>Jika kalian ingin tutorial-tutorial atau seputaran IT, Kalian bisa visit chanel warung cyber.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="fade-up" data-aos-delay="600">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-world"></i></div>
+              <h4><a href="">Situs MrTamfanX</a></h4>
+              <p>Jika kalian ingin mencari tau tentang mrtamfanx, kalian bisa ngegoogling.</p>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team">
+      <div class="container">
+
+        <div class="section-title">
+          <span>Profil Admin</span>
+          <h2>Profil Admin</h2>
+          <p>Dibawah ini adalah profil-profil admin mrtamfanx cyber team.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in">
+            <div class="member">
+              <img src="assets/img/team/team-1.jpg" alt="">
+              <h4>Prakasa_Jr64</h4>
+              <span>Leader</span>
+              <p>
+                Jangan Lupa Kunjungi Terus Website Kami : www.mrtamfanxcyberteam.my.id
+              </p>
+              <div class="social">
+                <a href=""><i class="icofont-twitter"></i></a>
+                <a href=""><i class="icofont-facebook"></i></a>
+                <a href=""><i class="icofont-instagram"></i></a>
+                <a href=""><i class="icofont-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in">
+            <div class="member">
+              <img src="assets/img/team/team-2.jpg" alt="">
+              <h4>Ibrahim</h4>
+              <span>Owner&Founder</span>
+              <p>
+                Jangan Lupa Kunjungi Terus Website Kami : www.mrtamfanxcyberteam.my.id
+              </p>
+              <div class="social">
+                <a href=""><i class="icofont-twitter"></i></a>
+                <a href=""><i class="icofont-facebook"></i></a>
+                <a href=""><i class="icofont-instagram"></i></a>
+                <a href=""><i class="icofont-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+    </section><!-- End Team Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <span>Contact</span>
+          <h2>Contact</h2>
+          <p>Berikutnya dibawah ini adalah contact dan alamat kami.</p>
+        </div>
+
+        <div class="row" data-aos="fade-up">
+          <div class="col-lg-6">
+            <div class="info-box mb-4">
+              <i class="bx bx-map"></i>
+              <h3>Alamat Kami ?</h3>
+              <p>DKI Jakarta, Jakarta utara, Pademangan barat, JL Budi Mulia Rt14 / Rw12 No 67B</p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="info-box  mb-4">
+              <i class="bx bx-envelope"></i>
+              <h3>Email Kami ?</h3>
+              <p>admin@mrtamfanxcyberteam.my.id</p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="info-box  mb-4">
+              <i class="bx bx-phone-call"></i>
+              <h3>Hubungi Kami ?</h3>
+              <p>+62 857-8041-1404</p>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="row" data-aos="fade-up">
+
+          <div class="col-lg-6 ">
+            <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps?q=-6.132300,106.837100&shorturl=1" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
+          </div>
+
+          <div class="col-lg-6">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="form-row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Nama kamu" data-rule="minlen:4" data-msg="Tolong isi nama kamu yang benar" />
+                  <div class="validate"></div>
+                </div>
+                <div class="col-md-6 form-group">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Email kamu" data-rule="email" data-msg="Tolong isi alamat email kamu yang benar" />
+                  <div class="validate"></div>
+                </div>
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subjek kamu" data-rule="minlen:4" data-msg="Tolong di isi subjek kamu yang benar" />
+                <div class="validate"></div>
+              </div>
+              <div class="form-group">
+                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Tolong isikan pesan kamu untuk kami" placeholder="Pesan kamu"></textarea>
+                <div class="validate"></div>
+              </div>
+              <div class="mb-3">
+                <div class="loading">Tunggu sebentar mas/mba</div>
+                <div class="error-message">Pesan kamu gagal di kirim ke MrTamfanX</div>
+                <div class="sent-message">Pesan kamu berhasil dikirim ke MrTamfanX, Terima kasih.</div>
+              </div>
+              <div class="text-center"><button type="submit">Kirim Pesan</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6">
+            <div class="footer-info">
+              <h3>MrTamfanX</h3>
+              <p>
+                Indonesia DKI Jakarta Jakarta Utara <br>
+                JL Budi Mulia Rt14 Rw12 No 67B <br><br>
+                <strong>Phone:</strong> +62 857-8041-1404<br>
+                <strong>Email:</strong> prakasap64@gmail.com<br>
+              </p>
+              <div class="social-links mt-3">
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Langganan MrTamfanX ?</h4>
+            <p>Klik subscribe untuk berlangganan ke mrtamfanx cyber team.</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>2018</span></strong>. MrTamfanX Cyber Team
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/day-multipurpose-html-template-for-free/ -->
+        Designed by <a href="https://www.mrtamfanxcyberteam.my.id/">Prakasa_Jr64</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5eb25f7c81d25c0e584936ab/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
